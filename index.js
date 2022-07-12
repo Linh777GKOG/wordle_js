e0e90b4a0b06dfd099e3fde6d699d1a2d3b0dfb0
Linh Vu <linh777gkog@gmail.com>
2022-07-12 23:39:35 +0700

    commit

--- index.js
+++ index.js
@@ -27,3 +27,26 @@ app.get('/word', (req, res) => {
       console.error(error);
     });
 });
+
+app.get('/check', (req, res) => {
+  const word = req.query.word;
+
+  const options = {
+    method: 'GET',
+    url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
+    params: { entry: word },
+    headers: {
+      'x-rapidapi-host': 'twinword-word-graph-dictionary.p.rapidapi.com',
+      'x-rapidapi-key': process.env.RAPID_API_KEY,
+    },
+  };
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data.result_msg);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
