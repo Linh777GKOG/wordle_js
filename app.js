d29d49f9d8d8b5b0acd1b26db49870c80b2c0ef8
Linh Vu <linh777gkog@gmail.com>
2022-07-12 23:41:19 +0700

    commit

--- app.js
+++ app.js
@@ -13,3 +13,34 @@ fetch('http://localhost:8000/word')
    .ch((err() =)> console.log(err));;
 };;
 getWordle();;
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
