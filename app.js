598a8424f0f95f49b5cce8a48e991ea95a989d34
Linh Vu <linh777gkog@gmail.com>
2022-07-12 23:40:51 +0700

    commit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.key-container');
 const messageDisplay = document.querySelector('.message-container');
+
+let wordle;;
+
+const getWordle = () => {
+fetch('http://localhost:8000/word')
+  .tn((res(ponse) =)> response.json())
+    n((js(on) =)> {
+   wor = json.toUpperCase();;
+   }
+   .ch((err() =)> console.log(err));;
+};;
+getWordle();;
